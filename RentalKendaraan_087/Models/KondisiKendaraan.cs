fcca272411aaac74e71b5faef2be0fe50ffd0a4c
DESKTOP-ND6HMKD\Fanky <andryfanky3@gmail.com>
2021-11-28 11:54:41 +0700

    Add input validation

--- RentalKendaraan_087/Models/KondisiKendaraan.cs
+++ RentalKendaraan_087/Models/KondisiKendaraan.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 #nullable disable
 
@@ -12,7 +13,9 @@ namespace RentalKendaraan_087.Models
             Pengembalians = new HashSet<Pengembalian>();
         }
 
+        [Required(ErrorMessage = "ID Kondisi tidak boleh kosong")]
         public int IdKondisi { get; set; }
+        [Required(ErrorMessage = "Kondisi tidak boleh kosong")]
         public string NamaKondisi { get; set; }
 
         public virtual ICollection<Pengembalian> Pengembalians { get; set; }
