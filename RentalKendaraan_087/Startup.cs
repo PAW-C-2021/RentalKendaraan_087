f4dab5739a34c0e791ee2cc00509cae660a682bc
DESKTOP-ND6HMKD\Fanky <andryfanky3@gmail.com>
2021-12-21 22:02:41 +0700

    Add upload file feature

--- RentalKendaraan_087/Startup.cs
+++ RentalKendaraan_087/Startup.cs
@@ -1,14 +1,17 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
+//using Microsoft.Extensions.Hosting;
+using RentalKendaraan_087.Models;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace RentalKendaraan_087
 {
@@ -22,14 +25,55 @@ namespace RentalKendaraan_087
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
+        [Obsolete]
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            //services.AddControllers();
 
-            services.AddDbContext<Models.RentKendaraanContext>(options => 
-            options.UseSqlServer(Configuration.GetConnectionString("Default")));
+            //services.Configure<CookiePolicyOptions>(options =>
+            //{
+            //    options.CheckConsentNeeded = context => true;
+            //    options.MinimumSameSitePolicy = SameSiteMode.None;
+            //});
 
-            services.AddControllersWithViews();
+            //services.AddSingleton<IFileProvider>(
+            //    new PhysicalFileProvider(
+            //        Path.Combine(Directory.GetCurrentDirectory(), "wwwroot")));
+
+            //services.AddDbContext<Models.RentKendaraanContext>(options => 
+            //options.UseSqlServer(Configuration.GetConnectionString("Default")));
+
+            //services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            //services.AddIdentity<IdentityUser, IdentityRole>().AddDefaultUI().AddEntityFrameworkStores<RentKendaraanContext>().AddDefaultTokenProviders();
+            //services.AddAuthorization(options =>
+            //{
+            //    options.AddPolicy("readonlypolicy",
+            //        builder => builder.RequireRole("Admin", "Manager", "Kasir"));
+            //    options.AddPolicy("writepolicy",
+            //        builder => builder.RequireRole("Admin", "Kasir"));
+            //    options.AddPolicy("editpolicy",
+            //        builder => builder.RequireRole("Admin", "Kasir"));
+            //    options.AddPolicy("deletepolicy",
+            //        builder => builder.RequireRole("Admin", "Kasir"));
+            //});
+            //services.AddScoped<Peminjaman>();
+            //services.AddScoped<Pengembalian>();
+
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Latest);
+            services.AddDbContext<Models.RentKendaraanContext>(options => options.UseSqlServer(Configuration.GetConnectionString("Default")));
+            services.AddSingleton<IFileProvider>(
+             new PhysicalFileProvider(
+                 Path.Combine(Directory.GetCurrentDirectory(), "wwwroot")));
+
+            //services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
